6236de5c2281273a614caf557a8dec4fc1e8d0d5
jinliang <123456>
2018-11-30 17:26:43 +0800

    first commit

---
+++ src/main/java/com/ws/webservice_client/WebserviceClientApplication.java
@@ -0,0 +1,12 @@
+package com.ws.webservice_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceClientApplication.class, args);
+	}
+}
